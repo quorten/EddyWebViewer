5ae56d79e5ff3dc5dc685d9b39ba5ac94be4578a
Andrew Makousky <mako0042@umn.edu>
2014-05-27 16:03:56 -0500

    Added perspective and mercator projections.

---
+++ src/base.js
@@ -0,0 +1,135 @@
+// TODO: Do all those class object existence checks...
+// "use strict";
+
+var oev;
+if (!oev)
+  oev = {};
+else if (typeof oev != 'object')
+  throw new
+    Error("Namespace conflict: oev already exists and is not an object.");
+
+oev.DEG2RAD = function(deg) {
+  return deg * Math.PI / 180;
+};
+
+oev.RAD2DEG = function(rad) {
+  return rad * 180 / Math.PI;
+};
+
+// Abstract class
+oev.MapProjector = {};
+
+/* Project a latitude-longitude polar coordinate to a map coordinate
+   for the current projection.
+
+   mapCoord.lat - latitude
+   mapCoord.lon - longitude
+
+   The coordinates must be normalized before this function call.
+
+   Returns map coordinates [-1..1] for both X and Y.
+
+   This is an abstract class that specifies the calling convention.
+   Use one of the concrete classes for the actual projection.
+*/
+oev.MapProjector.project = function(polCoord) {
+};
+
+oev.MapProjector.unproject = function(scrCoord) {
+};
+
+oev.EquirectMapProjector = {};
+
+oev.EquirectMapProjector.project = function(polCoord) {
+  var mapCoord = {};
+  mapCoord.y = polCoord.lat / 90;
+  mapCoord.x = polCoord.lon / 180;
+  return mapCoord;
+};
+
+oev.EquirectMapProjector.unproject = function(mapCoord) {
+  var polCoord = {};
+  polCoord.lat = mapCoord.y * 90;
+  polCoord.lon = mapCoord.x * 180;
+  return polCoord;
+};
+
+oev.MercatorMapProjector = {};
+
+oev.MercatorMapProjector.project = function(polCoord) {
+  var r = 1; // Radius
+  var mapCoord = {};
+  mapCoord.y = r * Math.log(Math.tan(Math.PI / 4 +
+				     oev.DEG2RAD(polCoord.lat) / 2));
+  mapCoord.x = polCoord.lon / 180;
+  return mapCoord;
+};
+
+oev.MercatorMapProjector.unproject = function(mapCoord) {
+  var r = 1; // Radius
+  var polCoord = {};
+  polCoord.lat = 2 * Math.atan(Math.exp(y / r)) - Math.PI / 2;
+  polCoord.lon = mapCoord.x * 180;
+  return polCoord;
+};
+
+oev.RobinsonMapProjector = {};
+oev.RobinsonMapProjector.table = {
+//  PLEN    PDFE         LAT
+  { 1.0000, 0.0000 }, // 00
+  { 0.9986, 0.0620 }, // 05
+  { 0.9954, 0.1240 }, // 10
+  { 0.9900, 0.1860 }, // 15
+  { 0.9822, 0.2480 }, // 20
+  { 0.9730, 0.3100 }, // 25
+  { 0.9600, 0.3720 }, // 30
+  { 0.9427, 0.4340 }, // 35
+  { 0.9216, 0.4958 }, // 40
+  { 0.8962, 0.5571 }, // 45
+  { 0.8679, 0.6176 }, // 50
+  { 0.8350, 0.6769 }, // 55
+  { 0.7986, 0.7346 }, // 60
+  { 0.7597, 0.7903 }, // 65
+  { 0.7186, 0.8435 }, // 70
+  { 0.6732, 0.8936 }, // 75
+  { 0.6213, 0.9394 }, // 80
+  { 0.5722, 0.9761 }, // 85
+  { 0.5322, 1.0000 }  // 90
+};
+
+oev.RobinsonMapProjector.project = function(polCoord) {
+  var table = oev.RobinsonProjector.table;
+  var alat = Math.abs(polCoord.lat);
+  var tbIdx1 = ~~Math.floor(alat / 5);
+  var tbIdx2 = ~~Math.ceil(alat / 5);
+  var interpol = alat % 5;
+  var plen = ((interpol * table[tbIdx1][0]) +
+	      ((1 - interpol) * table[tbIdx2][0])) / 2;
+  var pdfe = ((interpol * table[tbIdx1][1]) +
+	      ((1 - interpol) * table[tbIdx2][1])) / 2;
+  var mapCoord = {};
+  mapCoord.x = polCoord.lon * plen / 180;
+  mapCoord.y = pdfe * 0.5072;
+  if (polCoord.lat < 0)
+    mapCoord.y = -mapCoord.y;
+};
+
+// TODO: Need an efficient reverse lookup table.  Hash function.
+oev.RobinsonMapProjector.unproject = function(mapCoord) {
+};
+
+oev.W3MapProjector = {};
+
+oev.W3MapProjector.project = function(polCoord) {
+  var mapCoord = {};
+  var a = Math.acos(Math.cos(polCoord.lat) * Math.cos(polCoord.lon / 2));
+  var sinc_a = Math.sin(a) / a;
+  mapCoord.y = 1 / 2 * (polCoord.lat + Math.sin(polCoord.lat) / sinc_a);
+  mapCoord.x = 1 / 2 * (polCoord.lon * Math.cos(polCoord.lat) +
+			2 * Math.cos(polCoord.lat) * sin(polCoord.lon / 2) /
+			sinc_a);
+};
+
+oev.W3MapProjector.unproject = function(mapCoord) {
+  // Complicated reverse projection
+};

06fbba1a580dd0a92d28d1973c6526a7292cf427
Andrew Makousky <mako0042@umn.edu>
2014-06-24 17:02:08 -0500

    Improvements to GUI, worked on tracks optimizer.

--- src/tracksconv.c
+++ src/tracksconv.c
@@ -7,6 +7,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "../libs/cjs/exparray.h"
 
 struct InputEddy_tag {
@@ -24,9 +25,11 @@ struct SortedEddy_tag {
   float lon;
   unsigned date_index;
   unsigned unsorted_index;
-  unsigned prev; /* zero or index of previous eddy in track */
-  unsigned prev_date_index;
-  unsigned next; /* zero or index of next eddy in track */
+  /* Index of the next eddy in a track.  If this is less than the
+     index of the current eddy, then this is the last eddy in this
+     track, and the index is pointing to the first eddy in this
+     track.  */
+  unsigned next;
   unsigned next_date_index;
 };
 typedef struct SortedEddy_tag SortedEddy;
@@ -34,12 +37,17 @@ typedef struct SortedEddy_tag SortedEddy;
 EA_TYPE(InputEddy);
 EA_TYPE(InputEddy_array);
 EA_TYPE(SortedEddy);
+EA_TYPE(unsigned);
 
 InputEddy_array_array json_input;
 SortedEddy_array sorted_eddies;
+unsigned_array eddies_per_date;
+
+int se_date_cmp(const void *p1, const void *p2);
 
 int main(int argc, char *argv[]) {
   int retval;
+  unsigned etype; /* Eddy type */
 
   if (argc != 2) {
     fprintf(stderr, "Usage: tracksconv TYPE <INPUT >OUTPUT\n");
@@ -47,6 +55,11 @@ int main(int argc, char *argv[]) {
 	    "for an acyclonic tracks JSON.");
     return 1;
   }
+  etype = atoi(argv[1]);
+
+  EA_INIT(InputEddy_array, json_input, 16);
+  EA_INIT(SortedEddy, sorted_eddies, 16);
+  EA_INIT(unsigned, eddies_per_date, 16);
 
   /* Input data format: [ list of tracks ]
      track: [ list of eddies ]
@@ -61,7 +74,6 @@ int main(int argc, char *argv[]) {
     unsigned eddy_param_index = 0;
 
     scanf("["); nest_level++;
-    EA_INIT(InputEddy_array, json_input, 16);
     while (nest_level > 0) {
       int c;
       ungetc(c, stdin);
@@ -100,10 +112,33 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  /* Create an array that indexes on eddies rather than tracks.  Each
-     eddy will have a pointer to the previous and next eddy in the
-     track, or NULL at the beginning or end of the track.  */
-  EA_INIT(SortedEddy, sort_eddies);
+  { /* Create an array that indexes on eddies rather than tracks.  */
+    unsigned i;
+    for (i = 0; i < json_input.len; i++) {
+      unsigned j;
+      for (j = 0; j < json_input.d[i].len; j++) {
+	InputEddy *ieddy = json_input.d[i].d[j];
+	SortedEddy *seddy = sorted_eddies.d[sorted_eddies.len];
+	seddy->type = etype;
+	seddy->eddy_index = ieddy->eddy_index;
+	seddy->lat = ieddy->lat;
+	seddy->lon = ieddy->lon;
+	seddy->date_index = ieddy->date_index;
+	seddy->unsorted_index = sorted_eddies.len;
+	if (sorted_eddies.len > 0) {
+	  seddy = sorted_eddies.d[sorted_eddies.len-1];
+	  if (j < json_input.d[i].len - 1) {
+	    seddy->next = sorted_eddies.len;
+	    seddy->next_date_index = ieddy->date_index;
+	  } else {
+	    seddy->next = sorted_eddies.len - j;
+	    seddy->next_date_index = json_input.d[i].d[0].date_index;
+	  }
+	}
+	EA_ADD(sorted_eddies);
+      }
+    }
+  }
 
   { /* Cleanup the JSON input arrays now that we no longer need
        them.  */
@@ -114,28 +149,96 @@ int main(int argc, char *argv[]) {
   }
 
   /* Sort the eddies by date.  */
+  qsort(sorted_eddies.d, sorted_eddies.len, sizeof(SortedEddy),
+	se_date_cmp);
 
-  /* Find the number of eddies on each date index.  */
+  { /* The eddies are now grouped into series of contiguous chunks
+       where each date index in the chunk is identical.  Find the
+       number of eddies on each date index.  */
+    unsigned i;
+    unsigned last_date_index = 0;
+    unsigned cur_epd = 0; /* Current eddies per date */
+    for (i = 0; i < sorted_eddies.len; i++) {
+      if (sorted_eddies.d[i].date_index != last_date_index) {
+	EA_APPEND(eddies_per_date, cur_epd);
+	last_date_index = sorted_eddies.d[i].date_index;
+	cur_epd = 0;
+      }
+      cur_epd++;
+    }
+    EA_APPEND(eddies_per_date, cur_epd);
+  }
 
-  /* Determine the chunk size of the eddy data; that is, the number of
-     date indexes per chunk.  */
+  { /* Build a 2D kd-tree within each chunk from the latitude and
+       longitude of each eddy.  The top node of the kd-tree is stored
+       in the middle of the list so that tree traversal takes the same
+       form as a bsearch().  */
+    unsigned i;
+    unsigned j;
+    for (i = 0, j = 0; i < eddies_per_date.len; i++) {
+      kd_tree();
+    }
+  }
 
-  /* Build one kd-tree for each chunk of data.  */
+  { /* Perform two processing algorithms at this step:
 
-  /* No, one kd-tree per date index.  No chunking.  It is up to the
-     client to perform top-level kd-tree building.  */
+       1. Since all of sorting jumbled up the indexes of the eddies,
+          the index of the "next" eddy will have to be rematched.
+       2. Convert the indexes of the next eddy to relative indexes.
+          This translation will likely decrease the number of
+          characters in the output JSON (less data to download).  */
+    unsigned i;
+    for (i = 0; i < sorted_eddies.len; i++) {
 
-  /* Top-level kd-tree building is unnecessary.  Why?  Because the
-     runtime is still O(lg n).  Render linearly more points?  The user
-     expectation is linearly longer render time.  It's just a matter
-     of changing what the constant in the linear relationship is.
-     Chances are that the tree depth per date index will remain
-     constant.  */
+      /* Seek to the portion of the data with the date index of the
+	 next eddy, then use brute-force sequential search to find
+	 where the eddy with the unsorted index is located.  */
+      unsigned date_index = sorted_eddies.d[i].next_date_index;
+      unsigned frame_start = eddies_per_date.d[date_index];
+      unsigned frame_end;
+      unsigned j;
+      if (date_index < eddies_per_date.len)
+	frame_end = eddies_per_date.d[date_index+1];
+      else
+	frame_end = sorted_eddies.len;
+      for (j = frame_start; j < frame_end; j++) {
+	if sorted_eddies[j];
+	/* TODO FIXME need to keep track of cumulative total.  */
+      }
 
-  /* Convert the eddy previous and next pointers to relative
-     indexes.  */
+    }
+  }
 
-  /* Output the new JSON data.  */
+  { /* Output the new JSON data.  */
+    unsigned i;
+
+    /* First output the date indexes header.  */
+    printf("%10u,\n", eddies_per_date.len);
+    for (i = 0; i < eddies_per_date; i++) {
+      printf("%10u,", eddies_per_date.d[i]);
+      if (i % 4 == 0)
+	putchar('\n');
+    }
+    putchar('\n');
+
+    /* Next output the optimized eddy entries, each record padded to
+       the same byte width.  Newlines are written out at regular
+       intervals for readability.  */
+    for (i = 0; i < sorted_eddies.len - 1; i++) {
+      SortedEddy *seddy = sorted_eddies.d[i];
+      printf("[%1u,%5u,%10f,%10f,%10u],", seddy->type, seddy->eddy_index,
+	     seddy->lat, seddy->lon, seddy->next);
+      if (i % 4 == 0)
+	putchar('\n');
+    }
+
+    { /* Last iteration: no comma at the end of the data.  */
+      SortedEddy *seddy = sorted_eddies.d[i];
+      printf("[%1u,%5u,%10f,%10f,%10u]", seddy->type, seddy->eddy_index,
+	     seddy->lat, seddy->lon, seddy->next);
+      putchar('\n');
+    }
+  }
 
   retval = 0;
  cleanup:
@@ -146,5 +249,14 @@ int main(int argc, char *argv[]) {
     EA_DESTROY(json_input);
   }
   EA_DESTROY(sorted_eddies);
+  EA_DESTROY(eddies_per_date);
   return retval;
+}
+
+/* `qsort' comparison function for sorting eddy entries based off of
+   date.  */
+int se_date_cmp(const void *p1, const void *p2) {
+  const SortedEddy *se1 = (const SortedEddy*)p1;
+  const SortedEddy *se2 = (const SortedEddy*)p2;
+  return se1.date_index - se2.date_index;
 }

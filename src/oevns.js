ada7c345b7942a92e58658e3035e91f2ca8142d7
Andrew Makousky <mako0042@umn.edu>
2014-07-08 08:25:38 -0500

    Improved notes, minor GUI and code improvements.

--- src/oevns.js
+++ src/oevns.js
@@ -2,17 +2,17 @@
 
 /* See <http://spoken.fortybelow.ca/Browser_Scripting_101/#twenty_two>
    if you are unfamiliar with the following declaration.  */
-var global = this;
+var Global = this;
 
 /**
  * Ocean Eddies Web Viewer namespace.
  * @namespace
  */
-var oev;
+var OEV;
 
-if (!oev)
-  oev = {};
-else if (typeof(oev) != "object") {
-  throw new Error("Namespace conflict: oev already exists " +
+if (!OEV)
+  OEV = {};
+else if (typeof(OEV) != "object") {
+  throw new Error("Namespace conflict: OEV already exists " +
 		  "and is not an object.");
 }

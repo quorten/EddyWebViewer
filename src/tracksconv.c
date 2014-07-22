d89360aeae81750cda39ad1d15caea96eb6046da
Andrew Makousky <mako0042@umn.edu>
2014-07-22 12:16:08 -0500

    Finished kd-tree builder in tracksconv.c.

--- src/tracksconv.c
+++ src/tracksconv.c
@@ -70,35 +70,39 @@ unsigned_array date_chunk_starts;
 #define KD_DIMS 2
 SortedEddy_array kd_curdim[KD_DIMS+1];
 
-inline bool put_short_in_range(FILE *fout, unsigned value);
+bool put_short_in_range(FILE *fout, unsigned value);
 int parse_json(FILE *fp, unsigned eddy_type);
-inline void add_eddy(InputEddy *ieddy, unsigned eddy_type,
-		     bool start_of_track);
-inline int qs_date_cmp(const void *p1, const void *p2, void *arg);
-inline int qs_lat_cmp(const void *p1, const void *p2, void *arg);
-inline int qs_lon_cmp(const void *p1, const void *p2, void *arg);
-inline void qs_eddy_swap(void *p1, void *p2, void *arg);
+void add_eddy(InputEddy *ieddy, unsigned eddy_type,
+	      bool start_of_track);
+int qs_date_cmp(const void *p1, const void *p2, void *arg);
+int qs_lat_cmp(const void *p1, const void *p2);
+int qs_lon_cmp(const void *p1, const void *p2);
+void qs_eddy_swap(void *p1, void *p2, void *arg);
 void kd_eddy_move(SortedEddy *dest, SortedEddy *src);
-void kd_tree_build(unsigned start, size_t length);
+void kd_tree_build(unsigned begin_start, unsigned begin_length);
 
 void display_help(FILE *fout, const char *progname) {
-    fprintf(fout, "Usage: %s [-v] [-vv diag-file] [-x] [-o OUTPUT]\n"
+    fprintf(fout, "Usage: %s [OPTIONS] [-o OUTPUT]\n"
 	    "    [TYPE file TYPE file ...] [TYPE TYPE ... <INPUT] [>OUTPUT]\n",
 	    progname);
     fprintf(fout, "TYPE is 0 for an anticyclonic tracks JSON and 1 "
-	    "for a cyclonic tracks JSON.\n");
+	    "for a cyclonic tracks JSON.\n\n");
     fputs(
-"Specify -v for computational diagnostics, -vv for data diagnostics that are\n"
-"output to the given file, -x for extended output range (0x0000 to 0xf7fe),\n"
-"-o for output to a named file (standard output by default).\n"
-"Options must be specified in the order given above.\n", fout);
+"Options:\n"
+"  -v    Output computational diagnostics.\n"
+"  -vv diag-file    Output data diagnostics to the given file.\n"
+"  -x    Enable extended output range (0x0000 to 0xf7fe).\n"
+"  -nk   Disable kd-tree construction.\n"
+"  -o OUTPUT    Send output to a named file (standard output by default).\n",
+fout);
 }
 
 int main(int argc, char *argv[]) {
   int retval = 0;
   bool diag_proc = false;
+  bool build_kd = true;
   FILE *fdiag = NULL;
-  FILE *fout = NULL;
+  FILE *fout = stdout;
 
   if (argc < 2) {
     display_help(stderr, argv[0]);
@@ -109,30 +113,33 @@ int main(int argc, char *argv[]) {
   }
 
   argv++;
-  if (!strcmp(*argv, "-v")) {
-    diag_proc = true; argv++;
+
+#define FOPEN_ARGV_OR_ERROR(fp, mode) \
+  do { \
+    char *filename = *++argv; \
+    fp = fopen(filename, mode); \
+    if (fp == NULL) { \
+      fprintf(stderr, "Error: Could not open %s: %s\n", \
+	      filename, strerror(errno)); \
+      return 1; \
+    } \
+  } while(0)
+
+  while (*argv != NULL && (*argv)[0] == '-') {
+    if (!strcmp(*argv, "-v"))
+      diag_proc = true;
+    else if (!strcmp(*argv, "-vv"))
+      FOPEN_ARGV_OR_ERROR(fdiag, "wt");
+    else if (!strcmp(*argv, "-x"))
+      max_utf_range = true;
+    else if (!strcmp(*argv, "-o"))
+      FOPEN_ARGV_OR_ERROR(fout, "wb");
+    else if (!strcmp(*argv, "-nk"))
+      build_kd = false;
+    else
+      break;
+    argv++;
   }
-  if (*argv != NULL && !strcmp(*argv, "-vv")) {
-    char *diagname = *++argv; argv++;
-    fdiag = fopen(diagname, "wt");
-    if (fdiag == NULL) {
-      fprintf(stderr, "Error: Could not open %s: %s\n",
-	      diagname, strerror(errno));
-      return 1;
-    }
-  }
-  if (*argv != NULL && !strcmp(*argv, "-x"))
-    max_utf_range = true;
-  if (*argv != NULL && !strcmp(*argv, "-o")) {
-    char *outname = *++argv; argv++;
-    fout = fopen(outname, "wb");
-    if (fout == NULL) {
-      fprintf(stderr, "Error: Could not open %s: %s\n",
-	      outname, strerror(errno));
-      return 1;
-    }
-  } else
-    fout = stdout;
 
   if (*argv == NULL) {
     fputs("Error: Invalid command line.\n", stderr);
@@ -215,10 +222,15 @@ int main(int argc, char *argv[]) {
        where each date index in the chunk is identical.  Find the
        indexes that correspond to the start of each such chunk.  */
     unsigned i;
-    /* NOTE: Since the first date index is one, setting
-       last_date_index to zero will guarantee that the first iteration
-       adds a chunk start index.  */
+    /* NOTE: Since the first date index should be one, setting
+       `last_date_index' to zero will guarantee that the first
+       iteration adds a chunk start index.  */
     unsigned last_date_index = 0;
+    if (sorted_eddies.d[0].date_index == 0) {
+      fputs("Error: Date indexes may not equal zero.\n", stderr);
+      EA_APPEND(date_chunk_starts, 0);
+      retval = 1; /* goto cleanup; */
+    }
     for (i = 0; i < sorted_eddies.len; i++) {
       unsigned date_index_diff =
 	sorted_eddies.d[i].date_index - last_date_index;
@@ -226,13 +238,9 @@ int main(int argc, char *argv[]) {
 	EA_APPEND(date_chunk_starts, i);
 	last_date_index = sorted_eddies.d[i].date_index;
       } else if (date_index_diff != 0) {
-	if (last_date_index == 0) {
-	  fputs("Error: The first date index must be equal to one.\n", stderr);
-	} else {
-	  fputs("Error: Eddies must occupy every date index.\n", stderr);
-	  fprintf(stderr, "The eddies skip from date index %u to %u.\n",
-		  last_date_index, sorted_eddies.d[i].date_index);
-	}
+	fputs("Error: Every date index must be occupied by eddies.\n", stderr);
+	fprintf(stderr, "The eddies skip from date index %u to %u.\n",
+		last_date_index, sorted_eddies.d[i].date_index);
 	retval = 1; /* goto cleanup; */
       }
     }
@@ -244,8 +252,13 @@ int main(int argc, char *argv[]) {
   if (diag_proc)
     fprintf(stderr, "Done: %u date indexes.\n", date_chunk_starts.len - 1);
 
-  { /* Build the eddies in each date range into a kd-tree.  */
+  if (build_kd) {
+    /* Build kd-trees for each date index.  */
     unsigned i;
+
+    if (diag_proc)
+      fprintf(stderr, "Building kd-trees...\n");
+
     for (i = 0; i < date_chunk_starts.len - 1; i++) {
       SortedEddy *start = sorted_eddies.d + date_chunk_starts.d[i];
       unsigned length = date_chunk_starts.d[i+1] - date_chunk_starts.d[i];
@@ -268,10 +281,13 @@ int main(int argc, char *argv[]) {
       /* Build the actual kd-tree for this date range.  */
       kd_tree_build(0, length);
 
-      /* The finished kd-tree is located within kd_curdim[0].  Copy
-	 this back to the official location within
-	 `sorted_eddies'.  */
-      memcpy(start, kd_curdim[0].d, sizeof(SortedEddy) * length);
+      { /* Copy the finished kd-tree back to the official location
+	   within `sorted_eddies', rebasing the pointers as
+	   necessary.  */
+	unsigned j;
+	for (j = 0; j < length; j++)
+	  kd_eddy_move(start + j, &kd_curdim[0].d[j]);
+      }
     }
   }
 
@@ -549,7 +565,7 @@ int qs_date_cmp(const void *p1, const void *p2, void *arg) {
 }
 
 /* `qsorts_r()' latitude comparison function.  */
-int qs_lat_cmp(const void *p1, const void *p2, void *arg) {
+int qs_lat_cmp(const void *p1, const void *p2) {
   const SortedEddy *se1 = (const SortedEddy*)p1;
   const SortedEddy *se2 = (const SortedEddy*)p2;
   float cmp = se1->coords[0] - se2->coords[0];
@@ -561,7 +577,7 @@ int qs_lat_cmp(const void *p1, const void *p2, void *arg) {
 }
 
 /* `qsorts_r()' longitude comparison function.  */
-int qs_lon_cmp(const void *p1, const void *p2, void *arg) {
+int qs_lon_cmp(const void *p1, const void *p2) {
   const SortedEddy *se1 = (const SortedEddy*)p1;
   const SortedEddy *se2 = (const SortedEddy*)p2;
   float cmp = se1->coords[1] - se2->coords[1];
@@ -600,7 +616,8 @@ void qs_eddy_swap(void *p1, void *p2, void *arg) {
 }
 
 /* Similar to the `swap()' function above, this function handles
-   rearranging the list links during kd-tree construction.  */
+   rearranging the list links when an eddy gets moved to a new memory
+   address.  */
 void kd_eddy_move(SortedEddy *dest, SortedEddy *src) {
   if (src->next != NULL) src->next->prev = dest;
   if (src->prev != NULL) src->prev->next = dest;
@@ -612,103 +629,111 @@ void kd_eddy_move(SortedEddy *dest, SortedEddy *src) {
    in an efficient way.
 
    This function builds a 2D kd-tree based off of the latitudes and
-   longitudes of the given input eddies.  The finished kd-tree is
-   stored in `kd_curdim[0]'.  */
+   longitudes of the given input eddies.  The input should have been
+   presorted by each dimension into `kd_curdim'.  The finished kd-tree
+   is stored in `kd_curdim[0]'.  You must copy `kd_curdim[0]' to a
+   separate storage location using `kd_eddy_move()' to have a useful
+   data structure.  */
 
 typedef struct {
   unsigned start;
   unsigned length;
+  unsigned depth;
 } kd_stack_node;
 
-void kd_tree_build(unsigned start, unsigned length) {
-  return;
+#define KD_PUSH(istart, ilength, idepth) \
+  ((void) ((top->start = (istart)), (top->length = (ilength)), \
+	   (top->depth = (idepth)), ++top))
+#define	KD_POP(istart, ilength, idepth) \
+  ((void) (--top, (istart = top->start), (ilength = top->length), \
+	   (idepth = top->depth)))
 
+void kd_tree_build(unsigned begin_start, unsigned begin_length) {
   /* Note: The algorithms used to preprocess the data before this
      algorithm ensure that the number of eddies on a certain date
      index never equals zero.  Thus, it is not necessary to check if
      (length == 0).  */
 
-  unsigned cur_depth = 0;
-  unsigned push_pop = 0; /* num_pushes - num_pops */
+  unsigned start = begin_start;
+  unsigned length = begin_length;
+  unsigned depth = 0;
   kd_stack_node stack[QS_STACK_SIZE];
   kd_stack_node *top = stack;
 
-  QS_PUSH(NULL, NULL);
+  /* Set the `length' on the bogus first stack entry to 3 to simplify
+     the `KD_POP()' loop below.  */
+  KD_PUSH(0, 3, 0);
 
   while (QS_STACK_NOT_EMPTY) {
-    unsigned real_curdim; /* "Real" current dimension */
-    unsigned median, end = start + length;
+    unsigned real_curdim; /* "Real" current dimension (lat or lon) */
+    unsigned median, end;
     float median_val;
-    unsigned iter_dim;
-    /* End indexes of subarrays.  Note: In this usage, `right_subend'
-       includes the median element as the first element of the
-       subarray.  */
+    /* End indexes (index just beyond last element) of subarrays.
+       Note: In this usage, `right_subend' includes the median element
+       as the first element of the subarray.  */
     unsigned left_subend, right_subend;
+    bool median_moved;
     unsigned i, j;
 
-    if (length == 0) {
-      POP(start, length);
-      /* cur_depth--; */
-      if (push_pop == 1)
-	/* Still at the same depth.  */;
-      if (push_pop == 0)
-	cur_depth--;
-    }
-
     /* 1. Pick the median point at the current dimension.  */
-    real_curdim = (cur_depth) % 2;
-    median = start + length / 2;
-    /* Verify the median represents a >= division.  */
-    while (median > 0 &&
+    real_curdim = depth % 2;
+    /* TODO: Might need better median choosing equation.  */
+    median = start + length / 2; end = start + length;
+    /* Verify the median represents a `>=' division.  */
+    while (median > start &&
 	   kd_curdim[0].d[median-1].coords[real_curdim] ==
 	     kd_curdim[0].d[median].coords[real_curdim])
       median--;
     median_val = kd_curdim[0].d[median].coords[real_curdim];
 
     /* 2. Make a temporary copy of kd_curdim[0].  */
-    memcpy(kd_curdim[KD_DIMS-1].d, kd_curdim[0].d,
+    memcpy(kd_curdim[KD_DIMS].d + start, kd_curdim[0].d + start,
 	   sizeof(SortedEddy) * length);
 
     /* 3. Shift to the next current dimension by constructing the
-       `curdim + 1' points in kd_curdim[0].  Elements moved into the
-       first array must have their pointers rebased, since it will
-       contain the officially sorted copy when finished.  */
-    left_subend = 0; right_subend = median;
-    median_moved = false;
-    for (i = start; i < end; i++) {
-      int cmp = (int)kd_curdim[1][i] - (int)kd_curdim[0][median];
-      if (cmp < 0)
-	kd_eddy_move(&kd_curdim[1][i], &kd_curdim[0][i]);
-      else if (right_subend == median &&  == kd_curdim[0][median])
-	kd_eddy_move();
-    }
-
-    for (j = 1; j < KD_DIMS; j++) {
+       `curdim + 1' points in `curdim'.  Note that pointer rebasing
+       can be delayed until the kd-tree construction is finished,
+       since within a date index chunk, the pointers are either NULL
+       (point to themselves) or point outside the current date index
+       chunk.  */
+    for (j = 0; j < KD_DIMS; j++) {
+      left_subend = start; right_subend = median + 1;
+      median_moved = false;
       for (i = start; i < end; i++) {
-	if (kd_curdim[j][i] < kd_curdim[0][median]) {}
-      }
-    }
-
-    for (iter_dim = 0; iter_dim < KD_DIMS; iter_dim++) {
-      for (i = start; i < end; i++) {
-	if (kd_curdim[1][i] < kd_curdim[0][median]) {
-	  kd_eddy_move(kd_curdim[0][j], kd_curdim[1][i]);
-	  memcpy();
-	}
+	float cmp = kd_curdim[j+1].d[i].coords[real_curdim] - median_val;
+	if (cmp < 0) /* Left */
+	  memcpy(&kd_curdim[j].d[left_subend++], &kd_curdim[j+1].d[i],
+		 sizeof(SortedEddy));
+	else if (!median_moved && cmp == 0.0 &&
+		 !memcmp(&kd_curdim[j+1].d[i],
+			 &kd_curdim[0].d[median],
+			 sizeof(SortedEddy))) { /* Median */
+	  memcpy(&kd_curdim[j].d[median], &kd_curdim[j+1].d[i],
+		 sizeof(SortedEddy));
+	  median_moved = true;
+	} else /* Right */
+	  memcpy(&kd_curdim[j].d[right_subend++], &kd_curdim[j+1].d[i],
+		 sizeof(SortedEddy));
       }
     }
 
     { /* 4. Recurse on the left and right subarrays.  */
       unsigned left_len = left_subend - start;
       unsigned right_len = right_subend - (median + 1);
-      if (left_len > right_len ) {
+      depth++;
+      if (left_len > right_len) {
 	/* Push the larger left subarray.  */
-	PUSH(start, left_len);
-	start = median + 1; length = right_len; cur_depth++;
+	KD_PUSH(start, left_len, depth);
+	start = median + 1; length = right_len;
       } else {
 	/* Push the larger (or equal) right subarray.  */
-	PUSH(median + 1, right_len);
-	/* start = start; */ length = left_len; cur_depth++;
+	KD_PUSH(median + 1, right_len, depth);
+	/* start = start; */ length = left_len;
+      }
+
+      /* Handle trivial cases immediately.  */
+      while (length <= 1) {
+	KD_POP(start, length, depth);
       }
     }
   }

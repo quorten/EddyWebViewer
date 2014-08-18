a92f5b288741480b34a8358d2f6f0a08910b7f0f
Andrew Makousky <mako0042@umn.edu>
2014-08-18 15:14:39 -0500

    Improved vidlngen.sh: Get list of files from dates.dat.

--- src/vidlngen.sh
+++ src/vidlngen.sh
@@ -8,7 +8,8 @@ if [ -z "$CLASS" ]; then
   FMT=jpg
 fi
 
-FRAMELIST=`ls $CLASS | sed -e '/^format.json$/d' | sort`
+# FRAMELIST=`ls $CLASS | sed -e '/^format.json$/d' | sort`
+FRAMELIST=`cat dates.dat | sed -e 's/^\(.*\)$/ssh_\1.'$FMT'/g'`
 NUMFRAMES=`echo "$FRAMELIST" | wc -l`
 mkdir vidgen
 cd vidgen
@@ -17,5 +18,5 @@ while [ $i -lt $NUMFRAMES ]; do
   i=$((i + 1))
   FRAME=`echo "$FRAMELIST" | sed -ne ${i}p`
   FRNUM=`printf %05d $i`
-  ln -s ../$CLASS/$FRAME ssh_$FRNUM.$FMT
+  ln ../$CLASS/$FRAME ssh_$FRNUM.$FMT
 done
